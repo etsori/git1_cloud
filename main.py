48d266871e5ee8c960da3455a120bbf4165f5b6b
yandex <tsorievaeva@yandex.ru>
2024-11-18 19:10:08 +0300

    Добавили функцию calc

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 def printhi(name):
     print(name)
     print('Это изменения в файле')
+    # 2ed96812e152f87761b4db26d2245c5408ad7b83
     print('Еще ')
+    # 016dcbd6028ddd4c7b0d832613029b1fe8b90c7e
+
+def calc(a, b):
+    return a + b
 
 
 if __name__ == '__main__':
     printhi('Pycharm')
+    print(calc(10, 34))
+
